cd5df34ca2afdfda809d01a4c5b1faf2a5e80db8
Lindsey Heagy <lindseyheagy@gmail.com>
2018-07-31 14:26:56 -0700

    organization of notebooks, include info about what figures they reproduce in the header

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -6,14 +6,15 @@ NBDIR = os.path.sep.join(
     os.path.abspath(__file__).split(os.path.sep)[:-2] + ['notebooks']
 )
 IGNORE = [
-    1_DC_Flawed_Steel_Cased_Wells,
-    3_DC_Flawed_Steel_Cased_Wells_layer,
+    "1_DC_Flawed_Steel_Cased_Wells",
+    "3_DC_Flawed_Steel_Cased_Wells_layer",
+    "9_DC_Approximating_Steel_Cased_Wells_Cartesian"
 ]
 
 class TestNotebooks(unittest.TestCase):
 
     def test_notebooks(self):
-        Test = testipynb.TestNotebooks(directory=NBDIR, timeout=1800)
+        Test = testipynb.TestNotebooks(directory=NBDIR, timeout=3600)
         self.assertTrue(Test.run_tests())
 
 if __name__ == "__main__":
